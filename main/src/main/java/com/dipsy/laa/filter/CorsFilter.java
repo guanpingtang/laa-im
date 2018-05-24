a5aef106afbbcf3bd796f8dafea84e79f0a58465
tangguanping <1067418318@qq.com>
2018-05-24 19:30:29 +0800

    [add] 跨域支持

---
+++ main/src/main/java/com/dipsy/laa/filter/CorsFilter.java
@@ -0,0 +1,32 @@
+package com.dipsy.laa.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+        throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest reqs = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", reqs.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+} 
